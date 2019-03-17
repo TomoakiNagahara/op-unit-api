e69c4ea587cb18ac1b20bffda95f5ff03506cea1
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-03-18 01:10:49 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-api:/index.php
+ *
+ * @creation  2019-03-18
+ * @version   1.0
+ * @package   unit-api
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/Api.class.php');
