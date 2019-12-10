d8d8423ccdd8c798f1da0d842f994ba49932210b
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-10 17:59:20 +0900

    New: testcase/index.php - Separated from "module-testcase".

---
+++ testcase/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * unit-api:/testcase/index.php
+ *
+ * @created   2019-04-02
+ * @moved     2019-12-10   Separated from "module-testcase".
+ * @version   1.0
+ * @package   unit-api
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+/* @var $app OP\UNIT\App */
+/* @var $api OP\UNIT\Api */
+$api = $app->Unit('Api');
+
+//	...
+$scheme = 'http';
+$domain = $_SERVER['HTTP_HOST'];
+$path   = $app->URL('app:/api/');
+$url    = "{$scheme}://{$domain}{$path}";
+
+//	...
+$json = $app->Unit('Curl')->Get($url);
+
+//	...
+D( json_decode($json, true) );
