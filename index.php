81651a30d5cdc06f22983d8cf6e916cca406bafe
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-10 17:28:49 +0900

    NEW: ROMANCE DAWN - 2018

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * unit-api:/index.php
+ *
+ * @creation  2018-10-25
+ * @version   1.0
+ * @package   unit-api
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include('Api.class.php');
+
+//	...
+\OP\UNIT\Api::Init();
+
+//	...
+return true;
