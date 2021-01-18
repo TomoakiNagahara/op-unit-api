9fa82acc0bdaaf5d207c7a6740d7bdf63ade3dba
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2021-01-18 13:31:33 +0900

    Fix: Api :: _Init() | E_NOTICE

--- Api.class.php
+++ Api.class.php
@@ -63,8 +63,8 @@ class Api implements IF_UNIT
 
 		//	Init admin info.
 		self::Admin('endpoint', \OP\Unit::Instantiate('Router')->EndPoint());
-		self::Admin('get' , $_GET);
-		self::Admin('post', $_POST);
+		self::Admin('get' , $_GET  ?? null);
+		self::Admin('post', $_POST ?? null);
 
 		//	Switch display mime.
 		if( $_GET['html'] ?? null ){
