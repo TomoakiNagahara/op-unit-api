a069f05b0b53092e457c6967cf67a9658685e307
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-10 23:33:45 +0900

    New: testcase > json.php

---
+++ testcase/json.php
@@ -0,0 +1,20 @@
+<?php
+/** op-unit-api:/testcase/json.php
+ *
+ * @created   2024-02-06
+ * @package   op-unit-api
+ * @version   1.0
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/* @var $api \OP\UNIT\Api */
+$api = OP()->Unit('Api');
+
+//	...
+$api->Result('OK');
