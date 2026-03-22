e372ec946dd0ec88e23f394d8a162b49ab88394b
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-23 00:26:13 +0900

    Doc: Api.class.php | PHPDoc

--- Api.class.php
+++ Api.class.php
@@ -1,10 +1,9 @@
 <?php
-/**
- * unit-api:/Api.class.php
+/** op-unit-api:/Api.class.php
  *
- * @creation  2019-03-18
+ * @created   2019-03-18
  * @version   1.0
- * @package   unit-api
+ * @package   op-unit-api
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
